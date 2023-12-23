b7d01d71d13fc877352670afb4bc78378184d15f
sidpandagle <adi.pandagle@gmail.com>
2023-12-23 19:15:40 +0530

    Pred Done

--- home/views.py
+++ home/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render, HttpResponse
 import joblib
 import numpy as np  # Add this import for array manipulation
 
-model_path = 'static/model'
+model_path = 'static/rfc_model'
 model = joblib.load(model_path)
 print(f"Model loaded successfully from: {model_path}")
 
@@ -12,18 +12,18 @@ def Home(request):
 
 def Prediction(request):
     if request.method == 'POST':
-        age = int(request.POST.get('age'))  # Use a default value (0 in this case) if 'age' is None
-        gender = int(request.POST.get('gender'))
-        hypertension = int(request.POST.get('hypertension'))
-        heart_disease = int(request.POST.get('heart-disease'))
-        smoking_history = int(request.POST.get('smoking-history'))
-        bmi = float(request.POST.get('bmi'))  # Use a default value (0.0 in this case) if 'bmi' is None
+        age = int(request.POST.get('age'))  
+        gender = int(request.POST.get('gender', 0))
+        hypertension = int(request.POST.get('hypertension',0))
+        heart_disease = int(request.POST.get('heart-disease',0))
+        smoking_history = int(request.POST.get('smoking-history',0))
+        bmi = float(request.POST.get('bmi'))  
         hba1c_level = float(request.POST.get('hba1c-level'))
-        blood_glucose_level = float(request.POST.get('blood-glucose-level'))
+        blood_glucose_level = int(request.POST.get('blood-glucose-level',0))
 
         prediction_result = model.predict([[age, gender, hypertension, heart_disease, smoking_history, bmi, hba1c_level, blood_glucose_level]])
         output = {"output" : prediction_result}
-
+        print(output)
         return render(request, 'Prediction.html', output)
     else:
         return render(request, 'Prediction.html')
@@ -33,3 +33,5 @@ def About(request):
 
 def Contact(request):
     return render(request, 'Contact.html')
+
+
